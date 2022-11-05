dfdd0b18a35f0780d5fdd38442c8cb98e715c848
evgenija12345 <100587239+evgenija12345@users.noreply.github.com>
2022-11-05 21:58:49 +0100

    Add project files.

---
+++ VEZBA3/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿
+namespace VEZBA3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPublic = new System.Windows.Forms.RichTextBox();
+            this.txtPrivate = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPlain = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.txtEncrypt = new System.Windows.Forms.RichTextBox();
+            this.txtDecMsg = new System.Windows.Forms.RichTextBox();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.txtDecrypt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(364, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "RSA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(92, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Public Key";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(567, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Private key";
+            // 
+            // txtPublic
+            // 
+            this.txtPublic.Location = new System.Drawing.Point(27, 75);
+            this.txtPublic.Name = "txtPublic";
+            this.txtPublic.Size = new System.Drawing.Size(265, 96);
+            this.txtPublic.TabIndex = 3;
+            this.txtPublic.Text = "";
+            // 
+            // txtPrivate
+            // 
+            this.txtPrivate.Location = new System.Drawing.Point(449, 75);
+            this.txtPrivate.Name = "txtPrivate";
+            this.txtPrivate.Size = new System.Drawing.Size(269, 96);
+            this.txtPrivate.TabIndex = 4;
+            this.txtPrivate.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 197);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Текст";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(449, 196);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Енкриптиран текст";
+            // 
+            // txtPlain
+            // 
+            this.txtPlain.Location = new System.Drawing.Point(27, 222);
+            this.txtPlain.Name = "txtPlain";
+            this.txtPlain.Size = new System.Drawing.Size(265, 20);
+            this.txtPlain.TabIndex = 7;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(117, 248);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnEncrypt.TabIndex = 8;
+            this.btnEncrypt.Text = "Енкрипција";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // txtEncrypt
+            // 
+            this.txtEncrypt.Location = new System.Drawing.Point(27, 322);
+            this.txtEncrypt.Name = "txtEncrypt";
+            this.txtEncrypt.Size = new System.Drawing.Size(265, 96);
+            this.txtEncrypt.TabIndex = 9;
+            this.txtEncrypt.Text = "";
+            // 
+            // txtDecMsg
+            // 
+            this.txtDecMsg.Location = new System.Drawing.Point(449, 222);
+            this.txtDecMsg.Name = "txtDecMsg";
+            this.txtDecMsg.Size = new System.Drawing.Size(266, 96);
+            this.txtDecMsg.TabIndex = 10;
+            this.txtDecMsg.Text = "";
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(449, 335);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(104, 23);
+            this.btnDecrypt.TabIndex = 11;
+            this.btnDecrypt.Text = "Дескрипција";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // txtDecrypt
+            // 
+            this.txtDecrypt.Location = new System.Drawing.Point(449, 380);
+            this.txtDecrypt.Name = "txtDecrypt";
+            this.txtDecrypt.Size = new System.Drawing.Size(248, 20);
+            this.txtDecrypt.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtDecrypt);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.txtDecMsg);
+            this.Controls.Add(this.txtEncrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.txtPlain);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtPrivate);
+            this.Controls.Add(this.txtPublic);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox txtPublic;
+        private System.Windows.Forms.RichTextBox txtPrivate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPlain;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.RichTextBox txtEncrypt;
+        private System.Windows.Forms.RichTextBox txtDecMsg;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.TextBox txtDecrypt;
+    }
+}
+
---
+++ VEZBA3/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace VEZBA3
+{
+    public partial class Form1 : Form
+    {
+        UTF32Encoding ByteConverter = new UTF32Encoding();
+        RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static public byte[] RSAEncrypt(byte[] DataToEncrypt, RSAParameters RSAKeyInfo, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] encryptedData;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAKeyInfo);
+                    encryptedData = RSA.Encrypt(DataToEncrypt, DoOAEPPadding);
+                }
+                return encryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
+                RSA.FromXmlString(txtPublic.Text);
+                byte[] decrypted = System.Text.Encoding.Unicode.GetBytes(txtPlain.Text);
+                byte[] encrypted = RSA.Encrypt(decrypted, false);
+                txtEncrypt.Text = System.Convert.ToBase64String(encrypted);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        static public byte[] RSADecrypt(byte[] DataToDecrypt, RSAParameters RSAKeyInfo, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] decryptedData;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAKeyInfo);
+                    decryptedData = RSA.Decrypt(DataToDecrypt, DoOAEPPadding);
+                }
+                return decryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            if (txtDecMsg.Text == "")
+            {
+                MessageBox.Show("Prazen Vlez", "Greska", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                try
+                {
+                    CspParameters cspParam = new CspParameters();
+                    cspParam.Flags = CspProviderFlags.UseMachineKeyStore;
+                    RSACryptoServiceProvider RSA = new RSACryptoServiceProvider(cspParam);
+                    RSA.FromXmlString(txtPrivate.Text);
+                    byte[] encrypted = System.Convert.FromBase64String(txtDecMsg.Text);
+                    byte[] decrypted = RSA.Decrypt(encrypted, false);
+                    txtDecrypt.Text = System.Text.Encoding.Unicode.GetString(decrypted);
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+    }
+}
